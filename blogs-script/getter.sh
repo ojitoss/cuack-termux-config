6f09badb3006fe8ad5795b2d09fee7fbef110c5f
Jasil Gonzalez <jasil.gonzalez.dev@gmail.com>
2026-08-04 15:14:52 -0700

    add blogs-scripts

---
+++ blogs-script/getter.sh
@@ -0,0 +1,17 @@
+case $1 in
+  0)
+     echo "things-i-regretted-about-autag"
+     echo "Why is this variable?"
+     echo "Insane error management"
+     echo "Why is this variable?"
+     echo "Over-engineering"
+     echo "if/else management"
+     echo "DRY master"
+     echo "This breaks the project"
+     echo "Version controlling"
+     echo "Perfect english"
+     echo "Obssesed for some reason"
+     echo "Logo"
+     ;;
+  *) echo None;;
+esac
