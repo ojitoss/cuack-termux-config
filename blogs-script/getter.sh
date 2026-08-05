5742403f0dcc76b7812cab621ac3cc6c3f21beea
Jasil Gonzalez <jasil.gonzalez.dev@gmail.com>
2026-08-04 18:17:06 -0700

    refactor the getter to remove switch

--- blogs-script/getter.sh
+++ blogs-script/getter.sh
@@ -1,17 +1,34 @@
-case $1 in
-  0)
-     echo "things-i-regretted-about-autag"
-     echo "Why is this variable?"
-     echo "Insane error management"
-     echo "Why is this variable?"
-     echo "Over-engineering"
-     echo "if/else management"
-     echo "DRY master"
-     echo "This breaks the project"
-     echo "Version controlling"
-     echo "Perfect english"
-     echo "Obssesed for some reason"
-     echo "Logo"
-     ;;
-  *) echo None;;
-esac
+declare -a blogs_names
+declare -A blogs_sections
+
+blogs_names=(
+  "things-i-regretted-about-autag"
+)
+
+blogs_sections[0,0]="Why is this variable?"
+blogs_sections[0,1]="Insane error management"
+blogs_sections[0,2]="Over-engineering"
+blogs_sections[0,3]="if/else management"
+blogs_sections[0,4]="DRY master"
+blogs_sections[0,5]="This breaks the project"
+blogs_sections[0,6]="Version controlling"
+blogs_sections[0,7]="Perfect english"
+blogs_sections[0,8]="Obssesed for some reason"
+blogs_sections[0,9]="Logo"
+
+declare -a sorted_list
+
+for section in "${!blogs_sections[@]}"; do
+  IFS=","
+  read -r row col <<< $section
+
+  if [ $row -eq $1 ]; then
+    sorted_list[$col]="${blogs_sections[$section]}"
+  fi
+done
+
+echo "${blogs_names[$1]}"
+
+for section in "${sorted_list[@]}"; do
+  echo $section
+done
