d1d85a75297815bbac687c00f61bfc876c306653
Jasil Gonzalez <jasil.gonzalez.dev@gmail.com>
2026-08-07 15:27:24 -0700

    add seccond blog info to the getter

--- blogs-script/getter.sh
+++ blogs-script/getter.sh
@@ -2,7 +2,8 @@ declare -a blogs_names
 declare -A blogs_sections
 
 blogs_names=(
-  "things-i-regretted-about-autag"
+  "things-i-regretted-about-autag",
+  "autag-the-good-parts"
 )
 
 blogs_sections[0,0]="Why is this variable?"
@@ -15,6 +16,8 @@ blogs_sections[0,6]="Version controlling"
 blogs_sections[0,7]="Perfect english"
 blogs_sections[0,8]="Obssesed for some reason"
 blogs_sections[0,9]="Logo"
+
+blogs_sections[1,0]="/"
 
 declare -a sorted_list
 declare -i max=-1
